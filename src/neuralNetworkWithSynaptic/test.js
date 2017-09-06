ca8e11b6788c2b6532072d57f30fe37358658b71
cristiano <cso@cin.ufpe.br>
2017-09-06 17:09:36 -0300

    Testing

--- src/neuralNetworkWithSynaptic/test.js
+++ src/neuralNetworkWithSynaptic/test.js
@@ -4,3 +4,26 @@ var Neuron = synaptic.Neuron,
 	Network = synaptic.Network,
 	Trainer = synaptic.Trainer,
 	Architect = synaptic.Architect;
+
+function Perceptron(input, hidden, output)
+{
+	// create the layers
+	var inputLayer = new Layer(input);
+	var hiddenLayer = new Layer(hidden);
+	var outputLayer = new Layer(output);
+
+	// connect the layers
+	inputLayer.project(hiddenLayer);
+	hiddenLayer.project(outputLayer);
+
+	// set the layers
+	this.set({
+		input: inputLayer,
+		hidden: [hiddenLayer],
+		output: outputLayer
+	});
+}
+
+// extend the prototype chain
+Perceptron.prototype = new Network();
+Perceptron.prototype.constructor = Perceptron;
